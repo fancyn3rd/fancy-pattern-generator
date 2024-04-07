655e99c787579338d3c38a42c9d5ba077faaee73
Till G <>
2024-04-07 16:11:22 +0200

    Use full window size & clean up

--- src/canvas.js
+++ src/canvas.js
@@ -1,31 +1,30 @@
-
 import * as PIXI from "pixi.js";
 
-import { 
+import {
   colors,
   shapeTypes,
   blendModes,
   maxLineStrength,
-  maxLoop
-} from "./config.js"
+  maxLoop,
+  CANVAS_WIDTH,
+  CANVAS_HEIGHT,
+  MAX_OVERDRAWS,
+} from "./config.js";
 
-export const canvas = document.getElementById("canvas")
+export const canvas = document.getElementById("canvas");
 
-canvas.getContext("webgl",  {
-  preserveDrawingBuffer: true
-})
+canvas.getContext("webgl", {
+  preserveDrawingBuffer: true,
+});
 
 const container = new PIXI.Container();
 const RNDShape = new PIXI.Graphics();
 
-const app = new PIXI.Application(
-  canvas.width,
-  canvas.height, {
-    antialias: true,
-    view: canvas,
-    backgroundColor : colors[randomRange(0, colors.length)]
-  }
-);
+const app = new PIXI.Application(CANVAS_WIDTH, CANVAS_HEIGHT, {
+  antialias: true,
+  view: canvas,
+  backgroundColor: colors[randomRange(0, colors.length)],
+});
 
 app.stage.addChild(container);
 document.body.appendChild(app.view);
@@ -33,51 +32,56 @@ container.addChild(RNDShape);
 
 //*******************************************************************************************
 
-export function createPattern() { 
-  app.renderer.backgroundColor = colors[randomRange(0, colors.length)]
-  RNDShape.clear()
-    
-  for (let i=0; i<3; i++) {
-    initShape(RNDShape)
-    drawLoop(RNDShape)
+export function createPattern() {
+  app.renderer.backgroundColor = colors[randomRange(0, colors.length)];
+  RNDShape.clear();
+
+  const maxLoopDuration = randomRange(1, MAX_OVERDRAWS);
+
+  for (let i = 0; i < maxLoopDuration; i++) {
+    initShape(RNDShape);
+    drawLoop(RNDShape);
   }
 }
 
 function initShape(shape) {
-  shape.posX = randomRange(5, 10)
-  shape.posY = randomRange(5, 10)
-  shape.sizeW = randomRange(10, 20)
-  shape.sizeH = randomRange(10, 20)
-  shape.distanceX = shape.sizeW*3
-  shape.distanceY = shape.sizeH*2
-  shape.type = shapeTypes[randomRange(0, shapeTypes.length)]
-  shape.lineStrength = randomRange(0,maxLineStrength)
-  shape.fillColor = colors[randomRange(0, colors.length)]
-  shape.blendMode = PIXI.BLEND_MODES[blendModes[randomRange(1, blendModes.length)]]
+  shape.posX = randomRange(5, 10);
+  shape.posY = randomRange(5, 10);
+  shape.sizeW = randomRange(10, 20);
+  shape.sizeH = randomRange(10, 20);
+  shape.distanceX = shape.sizeW * 3;
+  shape.distanceY = shape.sizeH * 2;
+  shape.type = shapeTypes[randomRange(0, shapeTypes.length)];
+  shape.lineStrength = randomRange(0, maxLineStrength);
+  shape.fillColor = colors[randomRange(0, colors.length)];
+  shape.blendMode =
+    PIXI.BLEND_MODES[blendModes[randomRange(1, blendModes.length)]];
 }
 
-
 function drawLoop(shape) {
-  for (let y = 0; y <= maxLoop; y ++) {
-    shape.beginFill(shape.fillColor, 1)
+  for (let y = 0; y <= maxLoop; y++) {
+    shape.beginFill(shape.fillColor, 1);
     for (let x = 0; x <= maxLoop; x++) {
-        shape.lineStyle(shape.lineStrength, colors[randomRange(0, colors.length)]);
-        draw(shape, y, x)
+      shape.lineStyle(
+        shape.lineStrength,
+        colors[randomRange(0, colors.length)]
+      );
+      draw(shape, y, x);
     }
   }
-  shape.endFill()
+  shape.endFill();
 }
 
 function draw(shape, countY, countX) {
-    shape["draw" + shape.type](
-        shape.posX + countX * shape.distanceX,
-        shape.posY + countY * shape.distanceY, 
-        shape.sizeW,
-        shape.sizeH,
-        shape.sizeW
-    );
+  shape["draw" + shape.type](
+    shape.posX + countX * shape.distanceX,
+    shape.posY + countY * shape.distanceY,
+    shape.sizeW,
+    shape.sizeH,
+    shape.sizeW
+  );
 }
 
 function randomRange(min, max) {
-  return Math.floor(Math.random() * (max - 1 - min + 1) + min)
+  return Math.floor(Math.random() * (max - 1 - min + 1) + min);
 }
--- src/config.js
+++ src/config.js
@@ -1,32 +1,24 @@
+export const maxLineStrength = 15;
+export const maxLoop = 60;
 
-export const maxLineStrength = 15
-export const maxLoop = 30
+export const CANVAS_WIDTH = window.innerWidth;
+export const CANVAS_HEIGHT = window.innerHeight;
+export const MAX_OVERDRAWS = 3;
 
 export const colors = [
-    "0xEE3B3B",
-    "0x800000",
-    "0xFF2400",
-    "0xFF5333",
-    "0xFFB90F",
-    "0xF0A804",
-    "0xADFF2F",
-    "0xA2CD5A",
-    "0x87c4e0",
-    "0x87ceeb",
-    "0xc8509b"
-]
-  
-export const shapeTypes = [
-    "Circle",
-    "Rect",
-    "Ellipse",
-    "Star",
-    "RoundedRect"
-]
-  
-export const blendModes = [
-    "NORMAL", 	
-    "ADD", 	
-    "MULTIPLY", 	
-    "SCREEN"
-]
+  "0xEE3B3B",
+  "0x800000",
+  "0xFF2400",
+  "0xFF5333",
+  "0xFFB90F",
+  "0xF0A804",
+  "0xADFF2F",
+  "0xA2CD5A",
+  "0x87c4e0",
+  "0x87ceeb",
+  "0xc8509b",
+];
+
+export const shapeTypes = ["Circle", "Rect", "Ellipse", "Star", "RoundedRect"];
+
+export const blendModes = ["NORMAL", "ADD", "MULTIPLY", "SCREEN"];
