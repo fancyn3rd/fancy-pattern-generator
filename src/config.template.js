ce144acdfaca3dd3cf30163d77544232da5d19d5
Till G <>
2026-09-13 15:58:39 +0200

    Add config template

---
+++ src/config.template.js
@@ -0,0 +1,25 @@
+export const maxLineStrength = 15;
+export const maxLoop = 60;
+export const doAnimate = true;
+
+export const CANVAS_WIDTH = window.innerWidth;
+export const CANVAS_HEIGHT = window.innerHeight;
+export const MAX_OVERDRAWS = 3;
+
+export const colors = [
+  "0xEE3B3B",
+  "0x800000",
+  "0xFF2400",
+  "0xFF5333",
+  "0xFFB90F",
+  "0xF0A804",
+  "0xADFF2F",
+  "0xA2CD5A",
+  "0x87c4e0",
+  "0x87ceeb",
+  "0xc8509b",
+];
+
+export const shapeTypes = ["Circle", "Rect", "Ellipse", "Star", "RoundedRect"];
+
+export const blendModes = ["NORMAL", "ADD", "MULTIPLY", "SCREEN"];
